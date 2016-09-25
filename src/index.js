220a2304dca261db8bd9fbb600e629ed787e1648
Oscar Mejia <oscmejia@vovsolutions.com>
2016-09-25 09:39:39 -0500

    registration form UI finished

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
+// Classes
+import initDb from './db';
+import middleware from './middleware';
+import api from './api';
+import config from './config.json';
+
 let app = express();
 app.server = http.createServer(app);
 
-// CORS middleware
+//app.use('/static', express.static('public'));
+app.use(express.static(__dirname + '/public'));
+
+
+// CORS middleware 
 app.use(cors({
     exposedHeaders: config.corsHeaders
 }));
@@ -17,6 +27,19 @@ app.use(bodyParser.json({
     limit: config.bodyLimit
 }));
 
-app.use('/static', express.static('public'));
+
+// Initialize db layer
+initDb(db => {
+
+    // Internal Middleware
+    app.use(middleware({ config, db }));
+
+    // API Router
+    app.use('/api', api({ config, db }));
+
+    app.server.listen(config.port);
+
+    console.log('Started server on port ${app.server.address().port}');
+});
 
 export default app;
